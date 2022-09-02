5b1a64296e81117b2ef090cb789263a00d328a11
Felipe O. Gomes <69019358+Felipepsii@users.noreply.github.com>
2022-09-01 22:21:51 -0300

    Aula :)

---
+++ Aula03/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+
+namespace Aula03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Usuario p = new Usuario("Felitops", "batatop");
+            Usuario q = new Usuario("Pepinos", "pokemon38");
+            Usuario d = new Usuario("Pepinos", "pokemon38");
+            UserBase myBase = new UserBase("userbase.txt");
+            myBase.AddUser(p);
+            myBase.AddUser(q);
+            myBase.AddUser(d);
+            myBase.Save();
+
+
+        }
+    }
+}
---
+++ Aula03/Servico.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+
+
+namespace Aula03
+{
+    class Usuario
+    {
+
+           static int UID = 0;
+        int id;
+
+        string username;
+        string hash;
+
+        
+        public Usuario(string u, string p){
+
+            id = getUID();
+            SetUsername(u);
+            ApplyHash(p);   
+        }
+
+        static int getUID(){
+            UID++;
+            return UID;
+        }
+
+        void ApplyHash(string password){
+            
+            //TODO aplicar hash (ver na aula passada 25/08)
+            hash = password;
+        }
+
+        public string GetUsername(){
+            return username;
+        }
+
+        public string GetHash(){
+            return hash;
+        }
+
+        public void SetUsername(string u){
+            username = u;
+        }
+
+                public string Serialize(){
+            return id + "," + username + "," + hash;
+        }
+    }
+
+    class UserBase{
+
+        string filename;
+        List<Usuario> usuarios;
+
+        public UserBase(String f)
+        {
+            usuarios = new List<Usuario>();
+            filename = f;
+            LoadUsers();
+        } 
+
+        void LoadUsers(){
+         //   TODO
+        }
+
+        public void AddUser(Usuario u){
+            usuarios.Add(u);
+        }
+
+        public void Save(){
+            string output = "";
+            foreach(Usuario u in usuarios){
+            output += u.Serialize()+ "\n";
+
+            }
+            File.WriteAllText(filename,output);
+        }
+    }
+
+
+
+ 
+
+
+}
