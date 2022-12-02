51b346023aafb829d36dbb539d94a73b934e0043
Felipe O. Gomes <69019358+Felipepsii@users.noreply.github.com>
2022-12-01 22:20:47 -0300

    resumo

---
+++ Resumo/Controllers/WeatherForecastController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Resumo.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpPost]        
+        public Pessoa Post(Pessoa usuario)
+        {  
+
+
+            usuario.nasc.AddYears(1);
+            /*
+
+            PESSOA GET
+
+            Pessoa p = new Pessoa();
+            p.nome = "Felpo";
+            p.nasc = new DateTime(2022,08,04);
+            return p;   
+
+            */
+
+            return usuario;
+          
+           
+        }
+    }
+}
---
+++ Resumo/WeatherForecast.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Resumo
+{
+
+
+    public class Pessoa{
+
+        public string nome {get; set;}
+        public DateTime nasc {get; set;} 
+    }
+}
